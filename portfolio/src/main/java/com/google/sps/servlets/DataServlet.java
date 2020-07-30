049d69c9db8ce12785f990772a551ead695a0ee5
remusne <68026023+remusne@users.noreply.github.com>
2020-07-30 15:45:34 +0300

    Add DataStore (#6)
    * Just a tutorial step.
    
    * Fetch tutorial.
    
    * Add comment section.
    
    * Fix servlet interaction problem. Now the feature is in functional state.
    
    * Fix coding style issues.
    
    * Add comment class. For now, it has only the text field.
    
    * Modify script to receive JSON format. Make functions for converting comments in JSON. Remove initialized field.
    
    * Use Gson class instead of string operations. Remove redundant code.
    
    * Remove debug message.
    
    * Add dependency in pom.xml file.
    
    * Add a DataStore and insert entities for each comment.
    
    * Load data from DataStore. Now old comments are available to the user.
    
    * Remove unnecessary comment.
    
    * Fix comment.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -24,28 +24,65 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import com.google.gson.Gson;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 
 /** Servlet that handles comments data.*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    private static ArrayList<Comment> comments = new ArrayList<Comment>();
-
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Query query = new Query("Comment");
+
+        // Obtain information form DataStore
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
+        
+        // Add every comment to the comment list
+        ArrayList<Comment> comments = new ArrayList<Comment>();
+        for (Entity commentEntity : results.asIterable()) {
+            String text = (String) commentEntity.getProperty("text");
+            String time = (String) commentEntity.getProperty("time");
+            String date = (String) commentEntity.getProperty("date");
+            String user = (String) commentEntity.getProperty("user");
+
+            comments.add(new Comment(text, time, date, user));
+        }
+
+        // Print all the comments stored in DataStore
         response.setContentType("text/json;");
         response.getWriter().println(buildJsonFromList(comments));
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Create a data base using DatastoreService
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        
         // Get new comment and add it to the list
         String text = getParameter(request, "comment", "");
         
-        // TODO Add properties to the comments (besides text)
+        // Add new comment to data base
         Comment com = new Comment(text, "", "", "");        
-        comments.add(com);
+        storeData(com, datastore);
 
         // Redirect back to the HTML page.
         response.sendRedirect("/index.html");
+    }
+
+    // Add a Comment entity to a DataStore
+    private void storeData(final Comment comment, DatastoreService datastore) {
+        Entity commentEntity = new Entity("Comment");
+
+        commentEntity.setProperty("text", comment.getText());
+        commentEntity.setProperty("time", comment.getTime());
+        commentEntity.setProperty("date", comment.getDate());
+        commentEntity.setProperty("user", comment.getUser());
+
+        datastore.put(commentEntity);
     }
 
     // Converts a Comment object into a JSON
